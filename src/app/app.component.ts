70b283e803c5be701ef9973ac057d4e202d3cdb4
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-16 19:58:28 -0300

    add layout inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { ITask } from './interfaces/ITask';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project-apex-angular';
+  tasks: ITask[] = [
+    {
+      id: 'asdasd',
+      title: 'Limpar minha casa',
+      completed: false,
+    },
+    {
+      id: 'bbb',
+      title: 'Cortar o cabelo',
+      completed: true,
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+const MaterialModules = [
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatCheckboxModule,
+  MatGridListModule,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...MaterialModules,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
